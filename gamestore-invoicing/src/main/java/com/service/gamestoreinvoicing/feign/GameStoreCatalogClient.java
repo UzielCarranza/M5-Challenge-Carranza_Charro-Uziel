f59d132ab8a3a7d4ceb504d8b5d068eebec8f6aa
UzielCarranza <uzielcarranzac97@gmail.com>
2022-10-30 16:12:03 -0400

    Added feign client to connect to the game store catalog API

--- gamestore-invoicing/src/main/java/com/service/gamestoreinvoicing/feign/GameStoreCatalogClient.java
+++ gamestore-invoicing/src/main/java/com/service/gamestoreinvoicing/feign/GameStoreCatalogClient.java
@@ -1,2 +1,10 @@
-package com.service.gamestoreinvoicing.feign;public interface GameStoreCatalogClient {
+package com.service.gamestoreinvoicing.feign;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "gamestore-catalog")
+public interface GameStoreCatalogClient {
+
+        @GetMapping("/console")
+        public String getTest();
 }
