c76e2501d65492fe39a6004dfa659ce1e9584fb7
UzielCarranza <uzielcarranzac97@gmail.com>
2022-10-30 14:17:42 -0400

    registrered services with eureka service client dependency

---
+++ gamestore-invoicing/src/test/java/com/service/gamestoreinvoicing/GamestoreInvoicingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.gamestoreinvoicing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamestoreInvoicingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
