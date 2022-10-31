572e1c55b6e1ad3f1d6b3126d81eb44b38bdaa34
UzielCarranza <uzielcarranzac97@gmail.com>
2022-10-30 21:05:49 -0400

    added controllers to hanlde http requests

--- gamestore-catalog/src/main/java/com/service/gamestorecatalog/controller/ConsoleController.java
+++ gamestore-catalog/src/main/java/com/service/gamestorecatalog/controller/ConsoleController.java
@@ -1,72 +1,78 @@
 package com.service.gamestorecatalog.controller;
 
+import com.service.gamestorecatalog.service.ConsoleService;
+import com.service.gamestorecatalog.viewModel.ConsoleViewModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/console")
 @CrossOrigin(origins = {"http://localhost:3000"})
 public class ConsoleController {
 
-    @GetMapping
-    public void test(){
-        System.out.println("this is a test");
+    @Autowired
+    private ConsoleService consoleService;
+
+    public ConsoleController(ConsoleService consoleService) {
+        this.consoleService = consoleService;
     }
 
-//    @Autowired
-//    GameStoreServiceLayer service;
-//
-//    @PostMapping
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public ConsoleViewModel createConsole(@RequestBody @Valid ConsoleViewModel consoleViewModel) {
-//        consoleViewModel = service.createConsole(consoleViewModel);
-//        return consoleViewModel;
-//    }
-//
-//    @GetMapping("/{id}")
-//    @ResponseStatus(HttpStatus.OK)
-//    public ConsoleViewModel getConsole(@PathVariable("id") long consoleId) {
-//        ConsoleViewModel consoleViewModel = service.getConsoleById(consoleId);
-//        if (consoleViewModel == null) {
-//            throw new IllegalArgumentException("Console could not be retrieved for id " + consoleId);
-//        } else {
-//            return consoleViewModel;
-//        }
-//    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ConsoleViewModel createConsole(@RequestBody @Valid ConsoleViewModel consoleViewModel) {
+        consoleViewModel = consoleService.createConsole(consoleViewModel);
+        return consoleViewModel;
+    }
 
-//    @PutMapping
-//    @ResponseStatus(HttpStatus.NO_CONTENT)
-//    public void updateConsole(@RequestBody @Valid ConsoleViewModel consoleViewModel) {
-//
-//        if (consoleViewModel==null || consoleViewModel.getId()< 1) {
-//            throw new IllegalArgumentException("Id in path must match id in view model");
-//        } else if (consoleViewModel.getId() > 0) {
-//            service.updateConsole(consoleViewModel);
-//        }
-//    }
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ConsoleViewModel getConsole(@PathVariable("id") long consoleId) {
+        ConsoleViewModel consoleViewModel = consoleService.getConsoleById(consoleId);
+        if (consoleViewModel == null) {
+            throw new IllegalArgumentException("Console could not be retrieved for id " + consoleId);
+        } else {
+            return consoleViewModel;
+        }
+    }
 
-//    @DeleteMapping("/{id}")
-//    @ResponseStatus(HttpStatus.NO_CONTENT)
-//    public void deleteConsole(@PathVariable("id") long consoleId) {
-//        service.deleteConsole(consoleId);
-//    }
-//
-//    @GetMapping("/manufacturer/{manufacturer}")
-//    @ResponseStatus(HttpStatus.OK)
-//    public List<ConsoleViewModel> getConsoleByManufacturer(@PathVariable("manufacturer") String manu) {
-//        List<ConsoleViewModel> cvmByManufacturer = service.getConsoleByManufacturer(manu);
-//        if (cvmByManufacturer == null || cvmByManufacturer.isEmpty()) {
-//            throw new IllegalArgumentException("No consoles, manufactured by " + manu + ", were found");
-//        } else
-//            return cvmByManufacturer;
-//    }
-//
-//    @GetMapping()
-//    @ResponseStatus(HttpStatus.OK)
-//    public List<ConsoleViewModel> getAllConsoles() {
-//        List<ConsoleViewModel> cvmByManufacturer = service.getAllConsoles();
-//        if (cvmByManufacturer == null || cvmByManufacturer.isEmpty()) {
-//            throw new IllegalArgumentException("No consoles were found");
-//        } else
-//            return cvmByManufacturer;
-//    }
+    @PutMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateConsole(@RequestBody @Valid ConsoleViewModel consoleViewModel) {
+
+        if (consoleViewModel == null || consoleViewModel.getId() < 1) {
+            throw new IllegalArgumentException("Id in path must match id in view model");
+        } else if (consoleViewModel.getId() > 0) {
+            consoleService.updateConsole(consoleViewModel);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteConsole(@PathVariable("id") long consoleId) {
+        consoleService.deleteConsole(consoleId);
+    }
+
+    @GetMapping("/manufacturer/{manufacturer}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ConsoleViewModel> getConsoleByManufacturer(@PathVariable("manufacturer") String manu) {
+        List<ConsoleViewModel> cvmByManufacturer = consoleService.getConsoleByManufacturer(manu);
+        if (cvmByManufacturer == null || cvmByManufacturer.isEmpty()) {
+            throw new IllegalArgumentException("No consoles, manufactured by " + manu + ", were found");
+        } else
+            return cvmByManufacturer;
+    }
+
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public List<ConsoleViewModel> getAllConsoles() {
+        List<ConsoleViewModel> cvmByManufacturer = consoleService.getAllConsoles();
+        if (cvmByManufacturer == null || cvmByManufacturer.isEmpty()) {
+            throw new IllegalArgumentException("No consoles were found");
+        } else
+            return cvmByManufacturer;
+    }
 }
