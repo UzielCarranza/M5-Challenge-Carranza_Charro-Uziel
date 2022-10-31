04a82846a326825f3e083b494bf2ee69eccb1c42
UzielCarranza <uzielcarranzac97@gmail.com>
2022-10-30 21:01:29 -0400

    added routes to connect to the game store catalog database

--- gamestore-invoicing/src/main/java/com/service/gamestoreinvoicing/feign/GameStoreCatalogClient.java
+++ gamestore-invoicing/src/main/java/com/service/gamestoreinvoicing/feign/GameStoreCatalogClient.java
@@ -1,10 +1,27 @@
 package com.service.gamestoreinvoicing.feign;
+
+import com.service.gamestoreinvoicing.model.Console;
+import com.service.gamestoreinvoicing.model.Game;
+import com.service.gamestoreinvoicing.model.TShirt;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @FeignClient(name = "gamestore-catalog")
 public interface GameStoreCatalogClient {
 
-        @GetMapping("/console")
-        public String getTest();
+
+    @GetMapping("/console/{itemId}")
+    Optional<Console> findConsoleById(@PathVariable long itemId);
+
+
+
+    @GetMapping("/game/{itemId}")
+    Optional<Game> findGameById(@PathVariable long itemId);
+
+
+    @GetMapping("/tshirt/{itemId}")
+    Optional<TShirt> findTShirtById(@PathVariable long itemId);
 }
