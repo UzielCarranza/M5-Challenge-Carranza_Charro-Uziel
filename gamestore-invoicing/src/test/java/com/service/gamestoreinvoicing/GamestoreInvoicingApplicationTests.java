f940f1c04689bb7c066f2c054c8905022153ad76
UzielCarranza <uzielcarranzac97@gmail.com>
2022-11-01 22:42:59 -0400

    Added test cases

--- gamestore-invoicing/src/test/java/com/service/gamestoreinvoicing/GamestoreInvoicingApplicationTests.java
+++ gamestore-invoicing/src/test/java/com/service/gamestoreinvoicing/GamestoreInvoicingApplicationTests.java
@@ -6,6 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GamestoreInvoicingApplicationTests {
 
+
+//    IF TEST FAILS, MAKE SURE TO UPDATE PASSWORD ON APPLICATION PROPERTIES FILE FOUND UNDER THE RESOURCES FOLDER
+//    EX:
+//    spring.datasource.password=<password>
+
     @Test
     void contextLoads() {
     }
