c39954cb38325a8472e0405e15cf0fbcf25da65d
UzielCarranza <uzielcarranzac97@gmail.com>
2022-11-01 22:21:59 -0400

    added test cases

--- gamestore-catalog/src/test/java/com/service/gamestorecatalog/GamestoreCatalogApplicationTests.java
+++ gamestore-catalog/src/test/java/com/service/gamestorecatalog/GamestoreCatalogApplicationTests.java
@@ -6,6 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GamestoreCatalogApplicationTests {
 
+//    IF TEST FAILS, MAKE SURE TO UPDATE PASSWORD ON APPLICATION PROPERTIES FILE FOUND UNDER THE RESOURCES FOLDER
+//    EX:
+//    spring.datasource.password=<password>
     @Test
     void contextLoads() {
     }
