5b399471216c3257a83edf753910096ef4e9f99e
UzielCarranza <uzielcarranzac97@gmail.com>
2022-11-03 18:47:15 -0400

    removed duplicated code

--- gamestore-invoicing/src/test/java/com/service/gamestoreinvoicing/service/GameStoreInvoiceServiceLayerTest.java
+++ gamestore-invoicing/src/test/java/com/service/gamestoreinvoicing/service/GameStoreInvoiceServiceLayerTest.java
@@ -139,25 +139,12 @@ public class GameStoreInvoiceServiceLayerTest {
     //
     @Test(expected = IllegalArgumentException.class)
     public void shouldFailWhenCreateInvoiceInvalidItem() {
-        TShirt tShirt = new TShirt();
-        tShirt.setSize("Medium");
-        tShirt.setColor("Blue");
-        tShirt.setDescription("V-Neck");
-        tShirt.setPrice(new BigDecimal("19.99"));
-        tShirt.setQuantity(5);
 
-        InvoiceViewModel invoiceViewModel = new InvoiceViewModel();
-        invoiceViewModel.setName("John Jake");
-        invoiceViewModel.setStreet("street");
-        invoiceViewModel.setCity("Charlotte");
-        invoiceViewModel.setState("NC");
-        invoiceViewModel.setZipcode("83749");
+//        ACT
+//        INVOICE VIEW MODEL IS INSTANTIATED ON setUpInvoiceRepositoryMock()
+//        we pass an Item type of a shirt that doesnt exist
         invoiceViewModel.setItemType("nothing");
-        invoiceViewModel.setItemId(54);
-        invoiceViewModel.setQuantity(2);
-
         invoiceViewModel = service.createInvoice(invoiceViewModel);
-
     }
 
     @Test(expected = IllegalArgumentException.class)
