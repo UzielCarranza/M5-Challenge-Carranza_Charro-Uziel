4329f266374f7f5b9dd85eb7ddec2c54ba31d27c
UzielCarranza <uzielcarranzac97@gmail.com>
2022-11-02 21:28:58 -0400

    Added invoice service tests

--- gamestore-invoicing/src/test/java/com/service/gamestoreinvoicing/service/GameStoreInvoiceServiceLayerTest.java
+++ gamestore-invoicing/src/test/java/com/service/gamestoreinvoicing/service/GameStoreInvoiceServiceLayerTest.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.doReturn;
 
@@ -91,217 +91,208 @@ public class GameStoreInvoiceServiceLayerTest {
 
         InvoiceViewModel ivmfromService = service.getInvoice(invoiceViewModel.getId());
 
-        System.out.println(invoiceViewModel);
-        System.out.println(ivmfromService);
+        assertEquals(invoiceViewModel, ivmfromService);
+    }
+
+    @Test
+    public void shouldFindAllInvoices() {
+        InvoiceViewModel savedInvoice1 = new InvoiceViewModel();
+        savedInvoice1.setName("Sandy Beach");
+        savedInvoice1.setStreet("123 Main St");
+        savedInvoice1.setCity("any City");
+        savedInvoice1.setState("NY");
+        savedInvoice1.setZipcode("10016");
+        savedInvoice1.setItemType("T-Shirt");
+        savedInvoice1.setItemId(12);//pretending item exists with this id...
+        savedInvoice1.setUnitPrice(new BigDecimal("12.50"));//pretending item exists with this price...
+        savedInvoice1.setQuantity(2);
+        savedInvoice1.setSubtotal(savedInvoice1.getUnitPrice().multiply(new BigDecimal(savedInvoice1.getQuantity())));
+        savedInvoice1.setTax(savedInvoice1.getSubtotal().multiply(new BigDecimal("0.06")));
+        savedInvoice1.setProcessingFee(new BigDecimal("10.00"));
+        savedInvoice1.setTotal(savedInvoice1.getSubtotal().add(savedInvoice1.getTax()).add(savedInvoice1.getProcessingFee()));
+        savedInvoice1.setId(22);
+
+        InvoiceViewModel savedInvoice2 = new InvoiceViewModel();
+        savedInvoice2.setName("Rob Bank");
+        savedInvoice2.setStreet("888 Main St");
+        savedInvoice2.setCity("any town");
+        savedInvoice2.setState("NJ");
+        savedInvoice2.setZipcode("08234");
+        savedInvoice2.setItemType("Console");
+        savedInvoice2.setItemId(120);//pretending item exists with this id...
+        savedInvoice2.setUnitPrice(new BigDecimal("129.50"));//pretending item exists with this price...
+        savedInvoice2.setQuantity(1);
+        savedInvoice2.setSubtotal(savedInvoice2.getUnitPrice().multiply(new BigDecimal(savedInvoice2.getQuantity())));
+        savedInvoice2.setTax(savedInvoice2.getSubtotal().multiply(new BigDecimal("0.08")));
+        savedInvoice2.setProcessingFee(new BigDecimal("10.00"));
+        savedInvoice2.setTotal(savedInvoice2.getSubtotal().add(savedInvoice2.getTax()).add(savedInvoice2.getProcessingFee()));
+        savedInvoice2.setId(12);
+
+        InvoiceViewModel savedInvoice3 = new InvoiceViewModel();
+        savedInvoice3.setName("Sandy Beach");
+        savedInvoice3.setStreet("123 Broad St");
+        savedInvoice3.setCity("any where");
+        savedInvoice3.setState("CA");
+        savedInvoice3.setZipcode("90016");
+        savedInvoice3.setItemType("Game");
+        savedInvoice3.setItemId(19);//pretending item exists with this id...
+        savedInvoice3.setUnitPrice(new BigDecimal("12.50"));//pretending item exists with this price...
+        savedInvoice3.setQuantity(4);
+        savedInvoice3.setSubtotal(savedInvoice3.getUnitPrice().multiply(new BigDecimal(savedInvoice3.getQuantity())));
+        savedInvoice3.setTax(savedInvoice3.getSubtotal().multiply(new BigDecimal("0.09")));
+        savedInvoice3.setProcessingFee(BigDecimal.ZERO);
+        savedInvoice3.setTotal(savedInvoice3.getSubtotal().add(savedInvoice3.getTax()).add(savedInvoice3.getProcessingFee()));
+        savedInvoice3.setId(73);
+
+        List<InvoiceViewModel> currInvoices = new ArrayList<>();
+        currInvoices.add(savedInvoice1);
+        currInvoices.add(savedInvoice2);
+        currInvoices.add(savedInvoice3);
+
+        List<InvoiceViewModel> foundAllInvoices = service.getAllInvoices();
+
+        assertEquals(currInvoices.size(), foundAllInvoices.size());
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailCreateFindInvoiceWithBadState() {
+        TShirt tShirt = new TShirt();
+        tShirt.setId(99);
+        tShirt.setSize("Small");
+        tShirt.setColor("Red");
+        tShirt.setDescription("sleeveless");
+        tShirt.setPrice(new BigDecimal("400"));
+        tShirt.setQuantity(30);
+
+        InvoiceViewModel invoiceViewModel = new InvoiceViewModel();
+        invoiceViewModel.setName("John Jake");
+        invoiceViewModel.setStreet("street");
+        invoiceViewModel.setCity("Charlotte");
+        invoiceViewModel.setState("NY");
+        invoiceViewModel.setZipcode("83749");
+        invoiceViewModel.setItemType("T-Shirt");
+        invoiceViewModel.setItemId(99);
+        invoiceViewModel.setQuantity(2);
+
+        invoiceViewModel = service.createInvoice(invoiceViewModel);
+
+        InvoiceViewModel ivmfromService = service.getInvoice(invoiceViewModel.getId());
 
         assertEquals(invoiceViewModel, ivmfromService);
     }
 
-//    @Test
-//    public void shouldFindAllInvoices() {
-//        InvoiceViewModel savedInvoice1 = new InvoiceViewModel();
-//        savedInvoice1.setName("Sandy Beach");
-//        savedInvoice1.setStreet("123 Main St");
-//        savedInvoice1.setCity("any City");
-//        savedInvoice1.setState("NY");
-//        savedInvoice1.setZipcode("10016");
-//        savedInvoice1.setItemType("T-Shirt");
-//        savedInvoice1.setItemId(12);//pretending item exists with this id...
-//        savedInvoice1.setUnitPrice(new BigDecimal("12.50"));//pretending item exists with this price...
-//        savedInvoice1.setQuantity(2);
-//        savedInvoice1.setSubtotal(savedInvoice1.getUnitPrice().multiply(new BigDecimal(savedInvoice1.getQuantity())));
-//        savedInvoice1.setTax(savedInvoice1.getSubtotal().multiply(new BigDecimal("0.06")));
-//        savedInvoice1.setProcessingFee(new BigDecimal("10.00"));
-//        savedInvoice1.setTotal(savedInvoice1.getSubtotal().add(savedInvoice1.getTax()).add(savedInvoice1.getProcessingFee()));
-//        savedInvoice1.setId(22);
-//
-//        InvoiceViewModel savedInvoice2 = new InvoiceViewModel();
-//        savedInvoice2.setName("Rob Bank");
-//        savedInvoice2.setStreet("888 Main St");
-//        savedInvoice2.setCity("any town");
-//        savedInvoice2.setState("NJ");
-//        savedInvoice2.setZipcode("08234");
-//        savedInvoice2.setItemType("Console");
-//        savedInvoice2.setItemId(120);//pretending item exists with this id...
-//        savedInvoice2.setUnitPrice(new BigDecimal("129.50"));//pretending item exists with this price...
-//        savedInvoice2.setQuantity(1);
-//        savedInvoice2.setSubtotal(savedInvoice2.getUnitPrice().multiply(new BigDecimal(savedInvoice2.getQuantity())));
-//        savedInvoice2.setTax(savedInvoice2.getSubtotal().multiply(new BigDecimal("0.08")));
-//        savedInvoice2.setProcessingFee(new BigDecimal("10.00"));
-//        savedInvoice2.setTotal(savedInvoice2.getSubtotal().add(savedInvoice2.getTax()).add(savedInvoice2.getProcessingFee()));
-//        savedInvoice2.setId(12);
-//
-//        InvoiceViewModel savedInvoice3 = new InvoiceViewModel();
-//        savedInvoice3.setName("Sandy Beach");
-//        savedInvoice3.setStreet("123 Broad St");
-//        savedInvoice3.setCity("any where");
-//        savedInvoice3.setState("CA");
-//        savedInvoice3.setZipcode("90016");
-//        savedInvoice3.setItemType("Game");
-//        savedInvoice3.setItemId(19);//pretending item exists with this id...
-//        savedInvoice3.setUnitPrice(new BigDecimal("12.50"));//pretending item exists with this price...
-//        savedInvoice3.setQuantity(4);
-//        savedInvoice3.setSubtotal(savedInvoice3.getUnitPrice().multiply(new BigDecimal(savedInvoice3.getQuantity())));
-//        savedInvoice3.setTax(savedInvoice3.getSubtotal().multiply(new BigDecimal("0.09")));
-//        savedInvoice3.setProcessingFee(BigDecimal.ZERO);
-//        savedInvoice3.setTotal(savedInvoice3.getSubtotal().add(savedInvoice3.getTax()).add(savedInvoice3.getProcessingFee()));
-//        savedInvoice3.setId(73);
-//
-//        List<InvoiceViewModel> currInvoices = new ArrayList<>();
-//        currInvoices.add(savedInvoice1);
-//        currInvoices.add(savedInvoice2);
-//        currInvoices.add(savedInvoice3);
-//
-//        List<InvoiceViewModel> foundAllInvoices = service.getAllInvoices();
-//
-//        assertEquals(currInvoices.size(), foundAllInvoices.size());
-//    }
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailCreateFindInvoiceWithBadItemType() {
+        TShirt tShirt = new TShirt();
+        tShirt.setSize("Medium");
+        tShirt.setColor("Blue");
+        tShirt.setDescription("V-Neck");
+        tShirt.setPrice(new BigDecimal("19.99"));
+        tShirt.setQuantity(5);
+        InvoiceViewModel invoiceViewModel = new InvoiceViewModel();
+        invoiceViewModel.setName("John Jake");
+        invoiceViewModel.setStreet("street");
+        invoiceViewModel.setCity("Charlotte");
+        invoiceViewModel.setState("NC");
+        invoiceViewModel.setZipcode("83749");
+        invoiceViewModel.setItemType("Bad Item Type");
+        invoiceViewModel.setItemId(54);
+        invoiceViewModel.setQuantity(2);
 
-//
-//    @Test(expected = IllegalArgumentException.class)
-//    public void shouldFailCreateFindInvoiceWithBadState() {
-//        TShirt tShirt = new TShirt();
-//        tShirt.setId(99);
-//        tShirt.setSize("Small");
-//        tShirt.setColor("Red");
-//        tShirt.setDescription("sleeveless");
-//        tShirt.setPrice(new BigDecimal("400"));
-//        tShirt.setQuantity(30);
-//
-//        InvoiceViewModel invoiceViewModel = new InvoiceViewModel();
-//        invoiceViewModel.setName("John Jake");
-//        invoiceViewModel.setStreet("street");
-//        invoiceViewModel.setCity("Charlotte");
-//        invoiceViewModel.setState("NY");
-//        invoiceViewModel.setZipcode("83749");
-//        invoiceViewModel.setItemType("T-Shirt");
-//        invoiceViewModel.setItemId(99);
-//        invoiceViewModel.setQuantity(2);
-//
-//        invoiceViewModel = service.createInvoice(invoiceViewModel);
-//
-//        InvoiceViewModel ivmfromService = service.getInvoice(invoiceViewModel.getId());
-//
-//        assertEquals(invoiceViewModel, ivmfromService);
-//    }
-//
-//    @Test(expected = IllegalArgumentException.class)
-//    public void shouldFailCreateFindInvoiceWithBadItemType() {
-//        TShirt tShirt = new TShirt();
-//        tShirt.setSize("Medium");
-//        tShirt.setColor("Blue");
-//        tShirt.setDescription("V-Neck");
-//        tShirt.setPrice(new BigDecimal("19.99"));
-//        tShirt.setQuantity(5);
-////        tShirt = service.createTShirt(tShirt);
-//
-//        InvoiceViewModel invoiceViewModel = new InvoiceViewModel();
-//        invoiceViewModel.setName("John Jake");
-//        invoiceViewModel.setStreet("street");
-//        invoiceViewModel.setCity("Charlotte");
-//        invoiceViewModel.setState("NC");
-//        invoiceViewModel.setZipcode("83749");
-//        invoiceViewModel.setItemType("Bad Item Type");
-//        invoiceViewModel.setItemId(54);
-//        invoiceViewModel.setQuantity(2);
-//
-//        invoiceViewModel = service.createInvoice(invoiceViewModel);
-//
-//        InvoiceViewModel ivmfromService = service.getInvoice(invoiceViewModel.getId());
-//
-//        assertEquals(invoiceViewModel, ivmfromService);
-//    }
+        invoiceViewModel = service.createInvoice(invoiceViewModel);
+        System.out.println("here");
+        System.out.println(invoiceViewModel);
 
-//    @Test(expected = IllegalArgumentException.class)
-//    public void shouldFailCreateFindInvoiceWithNoInventory() {
-//        TShirt tShirt = new TShirt();
-//        tShirt.setSize("Medium");
-//        tShirt.setColor("Blue");
-//        tShirt.setDescription("V-Neck");
-//        tShirt.setPrice(new BigDecimal("19.99"));
-//        tShirt.setQuantity(5);
-////        tShirt = service.createTShirt(tShirt);
-//
-//        InvoiceViewModel invoiceViewModel = new InvoiceViewModel();
-//        invoiceViewModel.setName("John Jake");
-//        invoiceViewModel.setStreet("street");
-//        invoiceViewModel.setCity("Charlotte");
-//        invoiceViewModel.setState("NC");
-//        invoiceViewModel.setZipcode("83749");
-//        invoiceViewModel.setItemType("T-Shirt");
-//        invoiceViewModel.setItemId(54);
-//        invoiceViewModel.setQuantity(6);
-//
-//        invoiceViewModel = service.createInvoice(invoiceViewModel);
-//
-//        InvoiceViewModel ivmfromService = service.getInvoice(invoiceViewModel.getId());
-//
-//        assertEquals(invoiceViewModel, ivmfromService);
-//    }
-//
-//    @Test(expected = IllegalArgumentException.class)
-//    public void shouldFailWhenCreateInvoiceInvalidItem() {
-//        TShirt tShirt = new TShirt();
-//        tShirt.setSize("Medium");
-//        tShirt.setColor("Blue");
-//        tShirt.setDescription("V-Neck");
-//        tShirt.setPrice(new BigDecimal("19.99"));
-//        tShirt.setQuantity(5);
-////        tShirt = service.createTShirt(tShirt);
-//
-//        InvoiceViewModel invoiceViewModel = new InvoiceViewModel();
-//        invoiceViewModel.setName("John Jake");
-//        invoiceViewModel.setStreet("street");
-//        invoiceViewModel.setCity("Charlotte");
-//        invoiceViewModel.setState("NC");
-//        invoiceViewModel.setZipcode("83749");
-//        invoiceViewModel.setItemType("nothing");
-//        invoiceViewModel.setItemId(54);
-//        invoiceViewModel.setQuantity(2);
-//
-//        invoiceViewModel = service.createInvoice(invoiceViewModel);
-//
-//    }
+        InvoiceViewModel ivmfromService = service.getInvoice(invoiceViewModel.getId());
 
-//    @Test(expected = IllegalArgumentException.class)
-//    public void shouldFailWhenCreateInvoiceInvalidQuantity() {
-//        TShirt tShirt = new TShirt();
-//        tShirt.setSize("Medium");
-//        tShirt.setColor("Blue");
-//        tShirt.setDescription("V-Neck");
-//        tShirt.setPrice(new BigDecimal("19.99"));
-//        tShirt.setQuantity(5);
-////        tShirt = service.createTShirt(tShirt);
+        assertEquals(invoiceViewModel, ivmfromService);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailCreateFindInvoiceWithNoInventory() {
+        TShirt tShirt = new TShirt();
+        tShirt.setSize("Medium");
+        tShirt.setColor("Blue");
+        tShirt.setDescription("V-Neck");
+        tShirt.setPrice(new BigDecimal("19.99"));
+        tShirt.setQuantity(5);
+
+        InvoiceViewModel invoiceViewModel = new InvoiceViewModel();
+        invoiceViewModel.setName("John Jake");
+        invoiceViewModel.setStreet("street");
+        invoiceViewModel.setCity("Charlotte");
+        invoiceViewModel.setState("NC");
+        invoiceViewModel.setZipcode("83749");
+        invoiceViewModel.setItemType("T-Shirt");
+        invoiceViewModel.setItemId(54);
+        invoiceViewModel.setQuantity(6);
+
+        invoiceViewModel = service.createInvoice(invoiceViewModel);
+
+        InvoiceViewModel ivmfromService = service.getInvoice(invoiceViewModel.getId());
+        assertEquals(invoiceViewModel, ivmfromService);
+    }
 //
-//        InvoiceViewModel invoiceViewModel = new InvoiceViewModel();
-//        invoiceViewModel.setName("John Jake");
-//        invoiceViewModel.setStreet("street");
-//        invoiceViewModel.setCity("Charlotte");
-//        invoiceViewModel.setState("NC");
-//        invoiceViewModel.setZipcode("83749");
-//        invoiceViewModel.setItemType("T-Shirt");
-//        invoiceViewModel.setItemId(54);
-//        invoiceViewModel.setQuantity(0);
-//
-//        invoiceViewModel = service.createInvoice(invoiceViewModel);
-//    }
-//
-//    @Test(expected = NullPointerException.class)
-//    public void shouldFailWhenCreateInvoiceInvalidInvoiceMV() {
-//        TShirt tShirt = new TShirt();
-//        tShirt.setSize("Medium");
-//        tShirt.setColor("Blue");
-//        tShirt.setDescription("V-Neck");
-//        tShirt.setPrice(new BigDecimal("19.99"));
-//        tShirt.setQuantity(5);
-//
-//
-////        tShirt = gameStoreCatalogClient.createTShirt(tShirt);
-//
-//        InvoiceViewModel invoiceViewModel = null;
-//
-//        invoiceViewModel = service.createInvoice(invoiceViewModel);
-//    }
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailWhenCreateInvoiceInvalidItem() {
+        TShirt tShirt = new TShirt();
+        tShirt.setSize("Medium");
+        tShirt.setColor("Blue");
+        tShirt.setDescription("V-Neck");
+        tShirt.setPrice(new BigDecimal("19.99"));
+        tShirt.setQuantity(5);
+//        tShirt = service.createTShirt(tShirt);
+
+        InvoiceViewModel invoiceViewModel = new InvoiceViewModel();
+        invoiceViewModel.setName("John Jake");
+        invoiceViewModel.setStreet("street");
+        invoiceViewModel.setCity("Charlotte");
+        invoiceViewModel.setState("NC");
+        invoiceViewModel.setZipcode("83749");
+        invoiceViewModel.setItemType("nothing");
+        invoiceViewModel.setItemId(54);
+        invoiceViewModel.setQuantity(2);
+
+        invoiceViewModel = service.createInvoice(invoiceViewModel);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailWhenCreateInvoiceInvalidQuantity() {
+        TShirt tShirt = new TShirt();
+        tShirt.setSize("Medium");
+        tShirt.setColor("Blue");
+        tShirt.setDescription("V-Neck");
+        tShirt.setPrice(new BigDecimal("19.99"));
+        tShirt.setQuantity(5);
+
+        InvoiceViewModel invoiceViewModel = new InvoiceViewModel();
+        invoiceViewModel.setName("John Jake");
+        invoiceViewModel.setStreet("street");
+        invoiceViewModel.setCity("Charlotte");
+        invoiceViewModel.setState("NC");
+        invoiceViewModel.setZipcode("83749");
+        invoiceViewModel.setItemType("T-Shirt");
+        invoiceViewModel.setItemId(54);
+        invoiceViewModel.setQuantity(0);
+
+        invoiceViewModel = service.createInvoice(invoiceViewModel);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldFailWhenCreateInvoiceInvalidInvoiceMV() {
+        TShirt tShirt = new TShirt();
+        tShirt.setSize("Medium");
+        tShirt.setColor("Blue");
+        tShirt.setDescription("V-Neck");
+        tShirt.setPrice(new BigDecimal("19.99"));
+        tShirt.setQuantity(5);
+
+        InvoiceViewModel invoiceViewModel = null;
+
+        invoiceViewModel = service.createInvoice(invoiceViewModel);
+    }
 
     private void setUpInvoiceRepositoryMock() {
         invoiceRepository = mock(InvoiceRepository.class);
